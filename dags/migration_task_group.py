2a1fd3b79509cf6c7a5c0892c278a3e1a87da2d5
aloy99 <aloysiuschowhc@gmail.com>
2024-04-13 01:51:41 +0800

    Add stg mrt

--- dags/migration_task_group.py
+++ dags/migration_task_group.py
@@ -22,6 +22,12 @@ def migration():
         sql = "sql/tables/stg_resale_prices.sql"
     )
 
+    create_stg_mrts = PostgresOperator(
+        task_id = "create_stg_mrts",
+        postgres_conn_id = "resale_price_db",
+        sql = "sql/tables/stg_mrts.sql"
+    )
+
     create_int_mrts = PostgresOperator(
         task_id = "create_int_mrts",
         postgres_conn_id = "resale_price_db",
@@ -40,6 +46,6 @@ def migration():
         sql = "sql/tables/int_resale_prices.sql"
     )
 
-    create_pg_stg_schema >> create_stg_resale_price
+    create_pg_stg_schema >> create_stg_resale_price >> create_stg_mrts
     create_pg_warehouse_schema >> create_int_resale_price
     create_pg_warehouse_schema >> create_int_mrts >> create_int_nearest_mrts 
