97d453060c1e676e01efdd239fff38cbd0c6ef83
Pei Zhen <yappeizhen@gmail.com>
2024-04-13 18:09:15 +0800

    Pz/reorg db (#8)
    * shifted amenities into separate tables for better extendabilty
    
    * updated column for mrts in radius

--- dags/sql/tables/int_nearest_mrt.sql
+++ dags/sql/tables/int_nearest_mrt.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS warehouse.int_nearest_mrt (
   id SERIAL PRIMARY KEY,
-  flat_id INT NOT NULL,
-  mrt_id INT NOT NULL,
+  flat_id INT UNIQUE NOT NULL,
+  nearest_mrt_id INT NOT NULL,
+  num_mrts_within_radius INT,
   distance FLOAT NOT NULL,
-  FOREIGN KEY (mrt_id) REFERENCES warehouse.int_mrts(id),
-  UNIQUE (flat_id, mrt_id)
+  FOREIGN KEY (nearest_mrt_id) REFERENCES warehouse.int_mrts(id)
 );
 CREATE INDEX idx_flat_id ON warehouse.int_nearest_mrt(flat_id);
-CREATE INDEX idx_mrt_id ON warehouse.int_nearest_mrt(mrt_id);
+CREATE INDEX idx_nearest_mrt_id ON warehouse.int_nearest_mrt(nearest_mrt_id);
 CREATE INDEX idx_nearest_mrt_distance ON warehouse.int_nearest_mrt(distance);
