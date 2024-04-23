9fb3074b3fe0e470bb9d21b702586312fe4b533e
aloy99 <aloysiuschowhc@gmail.com>
2024-04-23 23:54:30 +0800

    Bug fixes, change dags to handle new scraper behaviour

--- dags/scraper/datagov/pri_school_scraper.py
+++ dags/scraper/datagov/pri_school_scraper.py
@@ -1,50 +1,32 @@
 import logging
-from typing import Any, Mapping, Generator, Tuple, Sequence
+from typing import Any, Mapping, Generator
+import pandas as pd
 
-import backoff
-from requests.exceptions import JSONDecodeError
-
-from scraper.base_scraper import BaseScraper
+from scraper.datagov.datagov_scraper import DatagovScraper
 from scraper.datagov.constants import (
-    DATAGOV_DATASETS_URL,
-    DATASETS_ENDPOINT,
     PRIMARY_SCHOOL_DATASET_ID,
     PRIMARY_SCHOOL_FIELDS)
 
 logger = logging.getLogger(__name__)
 
-class PriSchoolScraper(BaseScraper):
+class PriSchoolScraper(DatagovScraper):
 
     def __init__(self, headers: Mapping[str, str]):
-        super().__init__("", "", headers)
+        super().__init__(headers)
 
-    def scrape_dataset(self, dataset_id: str, params = {}) -> Generator[Mapping[str,Any], None, None]:
-        url = DATAGOV_DATASETS_URL + DATASETS_ENDPOINT + f'?resource_id={dataset_id}'
-        offset = 0
-        total = 1
-        while offset < total:
-            data, records = self.get_records(url, params)
-            offset = data['result'].get('offset', 0)
-            total = data['result'].get('total', 0)
-            url = DATAGOV_DATASETS_URL + data['result'].get('_links', {}).get('next').split("&filters")[0]
-            if records:
-                filtered_records = [self._row_handler(row) for row in records if row.get('mainlevel_code') == 'PRIMARY']
-                if filtered_records:  # Only yield if there are any valid records
-                    yield filtered_records
+    def scrape_dataset(self) -> pd.DataFrame:
 
-    
-    def _row_handler(self, row: Mapping[str, Any]) -> Sequence[Any]:
-        return tuple(row.get(field, None) for field in PRIMARY_SCHOOL_FIELDS)
+        file_path = self.download_data(PRIMARY_SCHOOL_DATASET_ID)
+        df = pd.read_csv(file_path)
+        cleaned_df = self.clean_df(df)
 
+        return cleaned_df
     
-    @backoff.on_exception(backoff.expo,
-                           KeyError,
-                           max_tries=3)
-    def get_records(self, url: str, params: str) -> Tuple[Mapping[str, Any], Mapping[str, Any]]:
-        response = self.get_req(url, "", params)
-        data = response.json()
-        return data, data['result']['records']
-    
+    def clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df[df['mainlevel_code'] == 'PRIMARY']
+        df = self.conform_dataframe_columns(df, PRIMARY_SCHOOL_FIELDS)
+        return df
+
     def run_scrape(self):
-        logger.info(f"Scraping dataset {PRIMARY_SCHOOL_DATASET_ID}")
-        yield from self.scrape_dataset(PRIMARY_SCHOOL_DATASET_ID)
+        logger.info("Scraping primary school dataset")
+        return self.scrape_dataset()
--- dags/scraper/datagov/supermarket_scraper.py
+++ dags/scraper/datagov/supermarket_scraper.py
@@ -1,47 +1,32 @@
 import logging
-from typing import Any, Mapping, Generator, Tuple, Sequence
-
-import backoff
+from typing import Any, Mapping, Generator
+import pandas as pd
 
 from scraper.base_scraper import BaseScraper
+from scraper.datagov.datagov_scraper import DatagovScraper
 from scraper.datagov.constants import (
-    DATAGOV_DATASETS_URL,
-    DATASETS_ENDPOINT,
     SUPERMARKET_DATASET_ID,
     SUPERMARKET_FIELDS)
 
 logger = logging.getLogger(__name__)
 
-class SupermarketScraper(BaseScraper):
+class SupermarketScraper(DatagovScraper):
 
     def __init__(self, headers: Mapping[str, str]):
-        super().__init__("", "", headers)
+        super().__init__(headers)
 
-    def scrape_dataset(self, dataset_id: str, params = {}) -> Generator[Mapping[str,Any], None, None]:
-        url = DATAGOV_DATASETS_URL + DATASETS_ENDPOINT + f'?resource_id={dataset_id}'
-        offset = 0
-        total = 1
-        while offset < total:
-            data, records = self.get_records(url, params)
-            offset = data['result'].get('offset', 0)
-            total = data['result'].get('total', 0)
-            url = DATAGOV_DATASETS_URL + data['result'].get('_links', {}).get('next').split("&filters")[0]
-            if records:
-                yield [self._row_handler(row) for row in records]
+    def scrape_dataset(self) -> pd.DataFrame:
 
-    
-    def _row_handler(self, row: Mapping[str, Any]) -> Sequence[Any]:
-        return tuple(row.get(field, None) for field in SUPERMARKET_FIELDS)
+        file_path = self.download_data(SUPERMARKET_DATASET_ID)
+        df = pd.read_csv(file_path)
+        cleaned_df = self.clean_df(df)
 
+        return cleaned_df
     
-    @backoff.on_exception(backoff.expo,
-                           KeyError,
-                           max_tries=3)
-    def get_records(self, url: str, params: str) -> Tuple[Mapping[str, Any], Mapping[str, Any]]:
-        response = self.get_req(url, "", params)
-        data = response.json()
-        return data, data['result']['records']
-    
+    def clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = self.conform_dataframe_columns(df, SUPERMARKET_FIELDS)
+        return df
+
     def run_scrape(self):
-        logger.info(f"Scraping dataset {SUPERMARKET_DATASET_ID}")
-        yield from self.scrape_dataset(SUPERMARKET_DATASET_ID)
+        logger.info("Scraping supermarket dataset")
+        return self.scrape_dataset()
